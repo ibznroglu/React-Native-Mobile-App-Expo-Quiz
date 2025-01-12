b7ebaa08aae2c6dac6ca070b3a8701ea3f00e037
ibznrMobile <ibznroglu@gmail.com>
2025-01-13 02:36:55 +0300

    quiz first-step

---
+++ src/questions.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+      question: "Türkiye'nin başkenti neresidir?",
+      options: ["İstanbul", "Ankara", "İzmir", "Bursa"],
+      correctAnswer: "Ankara",
+    },
+    {
+      question: "Kızıl Gezegen olarak bilinen gezegen hangisidir?",
+      options: ["Dünya", "Mars", "Jüpiter", "Venüs"],
+      correctAnswer: "Mars",
+    },
+    {
+      question: "Hamlet'in yazarı kimdir?",
+      options: ["Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"],
+      correctAnswer: "William Shakespeare",
+    },
+  ];
+  
+  export default questions;
